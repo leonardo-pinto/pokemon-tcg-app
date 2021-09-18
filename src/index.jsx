ad89493f031a4da34452620e3243a0d272099163
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-09-18 14:48:06 -0300

    feat: header containing sidebar

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,16 @@
+/* eslint-disable import/no-named-as-default-member */
+/* eslint-disable import/no-named-as-default */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root'),
 );
