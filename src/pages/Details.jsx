39a89f07756bb874930a6987f30533b9b2488106
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-09-19 13:50:25 -0300

    refact: details page with router

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,12 +1,37 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import CardDetails from '../components/CardDetails/CardDetails';
 
-function Details() {
+function Details(props) {
+  const { location: { state } } = props;
+
   return (
     <>
-      <CardDetails />
+      <CardDetails state={state} />
     </>
   );
 }
 
 export default Details;
+
+Details.propTypes = {
+  location: PropTypes.shape({
+    state: PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      images: PropTypes.shape({
+        small: PropTypes.string.isRequired,
+      }).isRequired,
+      evolvesFrom: PropTypes.string.isRequired,
+      evolvesTo: PropTypes.string,
+      artist: PropTypes.string.isRequired,
+      rarity: PropTypes.string.isRequired,
+      nationalPokedexNumbers: PropTypes.arrayOf(PropTypes.number),
+      cardmarket: PropTypes.shape({
+        updatedAt: PropTypes.string.isRequired,
+        prices: PropTypes.shape({
+          averageSellPrice: PropTypes.number.isRequired,
+        }),
+      }).isRequired,
+    }).isRequired,
+  }).isRequired,
+};
