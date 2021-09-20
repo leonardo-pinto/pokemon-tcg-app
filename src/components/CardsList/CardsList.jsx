faef91cdf4bfd20ad72646e93c7d85e7b9a91073
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-09-20 13:11:45 -0300

    fix: loading animation responsiveness

--- src/components/CardsList/CardsList.jsx
+++ src/components/CardsList/CardsList.jsx
@@ -14,7 +14,7 @@ function CardsList() {
     return (
       <div className="m-auto bg-purple-800 flex flex-wrap rounded-lg w-1/2 justify-center items-center">
         <svg
-          className="animate-spin -ml-1 mr-3 h-32 w-32 p-4 text-white"
+          className="animate-spin -ml-1 mr-3 h-20 w-20 p-4 sm:h-26 sm:w-26 md:h-30 md:w-30 text-white"
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
@@ -33,7 +33,7 @@ function CardsList() {
             d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
           />
         </svg>
-        <p className="text-white text-6xl p-4">Loading...</p>
+        <p className="text-white sm:text-4xl md:text-5xl text-2xl p-4">Loading...</p>
       </div>
     );
   }
